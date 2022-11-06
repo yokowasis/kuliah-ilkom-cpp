0245d3c5d5bbb890ee547973fe88f0eaaaa25326
yokowasis <yokowasis@gmail.com>
2022-11-06 14:35:52 +0800

    msg

---
+++ 013-tugas2_kelompok_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+  int x = 10;
+
+  /* cek apakah bilangan tersebut kelipatan 6 */ 
+  if (x % 6 == 0) {
+    cout << "Bilangan " << x << " adalah kelipatan 6" << endl;
+  } else {
+    cout << "Bilangan " << x << " bukan kelipatan 6" << endl;
+  }
+
+  return 0;
+}
