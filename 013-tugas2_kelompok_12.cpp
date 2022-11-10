6bdd6f2fc9d56c505a6d1d6dba7bfd9885c537b6
Your Name <you@example.com>
2022-11-10 17:00:24 +0800

    -

---
+++ 013-tugas2_kelompok_12.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+  double L,r;
+
+  cout << "Menghitung Luas Lingkaran \n";
+  cout << "----------------------------- \n";
+  cout << "Masukkan jari - hari : ";
+  cin >> r;
+
+  L = 22.0 / 7.0 * r * r;
+  cout << "Luas Lingkarang : " << L << endl;
+
+  return 0;
+}
